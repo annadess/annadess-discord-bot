d68b3a13004dc46b69a728e85f50f1d33f08ede6
annadess <a.bubnyak@gmail.com>
2018-07-01 20:06:02 +0200

    cleanup

--- bot.py
+++ bot.py
@@ -12,17 +12,25 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-def isCommand(message, command):
-    print('detected')
-    return message.content.startswith(prefix+message)
-    
+def isCommand(message,command):
+    return message.content.startswith(prefix+command)
     
 @asyncio.coroutine
 @client.event
 async def on_message(message):
-    if isCommand(message,'bestship'):
+    if isCommand(message,'test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif isCommand(message,'sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+    elif isCommand(message,'bestship'):
         await client.send_message(message.channel, 'Kuno x Wyn is definitely the best ship')
-    else:
-        print('undetected')
+        
 
 client.run(os.environ.get('BOT_TOKEN'))
