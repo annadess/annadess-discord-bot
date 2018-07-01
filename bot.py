55fdb424bacfd480cc23468b78e83e9e581053ab
annadess <a.bubnyak@gmail.com>
2018-07-01 16:19:29 +0200

    added new function

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import asyncio
 import os
 
 client = discord.Client()
+prefix = ','
 
 @client.event
 async def on_ready():
@@ -14,7 +15,7 @@ async def on_ready():
 @asyncio.coroutine
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith(prefix+'test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
@@ -22,8 +23,11 @@ async def on_message(message):
                 counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
+    elif message.content.startswith(prefix+'sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
+	elif message.content.startswith(prefix+'bestship'):
+		await client.send_message(message.channel, 'Kuno x Wyn is definitely the best ship')
+		
 
 client.run(os.environ.get('BOT_TOKEN'))
