09e569b0c656d13cc2e9b2582816ae207366b658
annadess <a.bubnyak@gmail.com>
2018-07-01 21:29:02 +0200

    cleanup

--- bot.py
+++ bot.py
@@ -18,19 +18,15 @@ def isCommand(message,command):
 @asyncio.coroutine
 @client.event
 async def on_message(message):
-    if isCommand(message,'test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif isCommand(message,'sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-    elif isCommand(message,'bestship'):
+    if isCommand(message,'bestship'):
         await client.send_message(message.channel, 'Kuno x Wyn is definitely the best ship')
-        
+    elif isCommand(message, 'writefile'):
+        with open("file.txt") as file:
+            f.write("sample")
+            await client.send_message(message.channel, 'written to file')
+    elif isCommand(message, 'readfile'):
+        with open("file.txt") as file:
+            content = f.read("sample")
+            await client.send_message(message.channel, content)        
 
 client.run(os.environ.get('BOT_TOKEN'))
