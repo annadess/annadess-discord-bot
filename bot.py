43f61e6b33151682d2ab308b653ec2237c82fa94
annadess <a.bubnyak@gmail.com>
2018-07-02 20:06:50 +0200

    Basic insert/update features

--- bot.py
+++ bot.py
@@ -18,29 +18,62 @@ async def on_ready():
 def isCommand(message,command):
     return message.content.startswith(prefix+command)
     
-@asyncio.coroutine
+def checkEntry(message):
+    return message.content.startswith('new') or message.content.startswith('edit')
+    
+def checkEdit(message):
+    if message.content.startswith('abort'):
+        return True
+    cur.execute("SELECT ID FROM MEMBERS")
+    query = cur.fetchall()
+    if int(message.content) in query:
+        return True
+    else:
+        return False
+        
+        @asyncio.coroutine
 @client.event
 async def on_message(message):
     if isCommand(message,'bestship'):
-        await client.send_message(message.channel, 'Kuno x Wyn is definitely the best ship')
+        await client.send_message(message.channel, 'Sir, the Kuno and Wyn pairing, is undoubtedly, the best pairing in this currently running role playing game.')
     elif isCommand(message, 'updateusers'):
-        sql = """INSERT INTO MEMBERS(username)
-             VALUES(%s);"""
         members_curr = message.server.members
         cur.execute("SELECT username FROM MEMBERS")
         members_stored = cur.fetchall()
         members_new = list(set(members_curr)-set(members_stored))
-        for member in members_new:
-            #try:
-            cur.execute(sql, [str(member)])
-            #except:
-                #print("error at")
-                #print(member.name)
-                #pass
+        if len(members_new)==0:
+            await client.send_message(message.channel, 'Sir, my database is up to date with the members of this server, no changes required.')
+        else:
+            await client.send_message(message.channel, 'It seems are new members sir.')
+            for member in members_new:
+                await client.send_message(message.channel, 'Would you like to add '+member.name()+' as a new entry, or would you like to replace an entry?')
+                await client.send_message(message.channel, '(type new or edit)')
+                response = await client.wait_for_message(author=message.author,check=checkEntry)
+                if response.content.startswith('new'):
+                    sql = """INSERT INTO MEMBERS(username)
+                    VALUES(%s);"""
+                    cur.execute(sql, [str(member)])
+                    await client.send_message(message.channel, 'Sucessfully added:')
+                    await client.send_message(message.channel, str(member))
+                else:
+                    await client.send_message(message.channel, 'These are your current members sir:')
+                    cur.execute("SELECT * FROM MEMBERS")
+                    rows = cur.fetchall()
+                    for r in rows:
+                        await client.send_message(message.channel, '`'+r+'`');
+                    await client.send_message(message.channel, 'Which username would you like to update? (type id number or abort)')
+                    response = await client.wait_for_message(author=message.author,check=checkEdit)
+                    if response.content.startswith('abort'):
+                        break
+                        await client.send_message(message.channel, 'Process aborted');
+                    else:
+                        sql = """UPDATE MEMBERS SET username=%s 
+                        WHERE ID=%d"""
+                        cur.execute(sql,str(member),int(response.content))
+                        await client.send_message(message.channel, 'Entry successfully updated');
+                        
         conn.commit()
-        await client.send_message(message.channel, 'Sucessfully added:')
-        await client.send_message(message.channel, [str(i) for i in members_new])
     elif isCommand(message, 'closedb'):
         conn.close()
-
+        
 client.run(os.environ.get('BOT_TOKEN'))
