22a32e72429ab4f7ca7c3cc303dad93c5ba93d3a
annadess <a.bubnyak@gmail.com>
2018-07-03 17:56:34 +0200

    slpitting functions into modules

--- database.py
+++ database.py
@@ -20,8 +20,8 @@ class Database:
     def checkEdit(self,message):
         if message.content.startswith('abort'):
             return True
-        cur.execute("SELECT ID FROM MEMBERS")
-        query = cur.fetchall()
+        self.cur.execute("SELECT ID FROM MEMBERS")
+        query = self.cur.fetchall()
         query = [i[0] for i in query]
         if int(message.content) in query:
             return True
@@ -32,53 +32,53 @@ class Database:
     async def updateusers(self,message):
         members_curr = message.server.members
         members_curr = [str(i) for i in members_curr]
-        cur.execute("SELECT username FROM MEMBERS")
-        members_stored = cur.fetchall()
+        self.cur.execute("SELECT username FROM MEMBERS")
+        members_stored = self.cur.fetchall()
         members_stored = [i[0] for i in members_stored]
         members_new = list(set(members_curr)-set(members_stored))
         if len(members_new)==0:
-            await client.send_message(message.channel, 'Sir, my database is up to date with the members of this server, no changes required.')
+            await self.client.send_message(message.channel, 'Sir, my database is up to date with the members of this server, no changes required.')
         else:
-            await client.send_message(message.channel, 'It seems there are new members sir.')
+            await self.client.send_message(message.channel, 'It seems there are new members sir.')
             for member in members_new:
-                await client.send_message(message.channel, 'Would you like to add '+member+' as a new entry, or would you like to replace an entry?')
-                await client.send_message(message.channel, '(type new or edit)')
-                response = await client.wait_for_message(author=message.author,check=checkEntry)
+                await self.client.send_message(message.channel, 'Would you like to add '+member+' as a new entry, or would you like to replace an entry?')
+                await self.client.send_message(message.channel, '(type new or edit)')
+                response = await self.client.wait_for_message(author=message.author,check=self.checkEntry)
                 if response.content.startswith('new'):
                     sql = """INSERT INTO MEMBERS(username)
                     VALUES(%s);"""
-                    cur.execute(sql, [str(member)])
-                    await client.send_message(message.channel, 'Sucessfully added:')
-                    await client.send_message(message.channel, str(member))
+                    self.cur.execute(sql, [str(member)])
+                    await self.client.send_message(message.channel, 'Sucessfully added:')
+                    await self.client.send_message(message.channel, str(member))
                 else:
-                    await client.send_message(message.channel, 'These are your current members sir:')
-                    cur.execute("SELECT * FROM MEMBERS")
-                    rows = cur.fetchall()
+                    await self.client.send_message(message.channel, 'These are your current members sir:')
+                    self.cur.execute("SELECT * FROM MEMBERS")
+                    rows = self.cur.fetchall()
                     for r in rows:
-                        await client.send_message(message.channel, '`'+str(r)+'`');
-                    await client.send_message(message.channel, 'Which username would you like to update? (type id number or abort)')
-                    response = await client.wait_for_message(author=message.author,check=checkEdit)
+                        await self.client.send_message(message.channel, '`'+str(r)+'`');
+                    await self.client.send_message(message.channel, 'Which username would you like to update? (type id number or abort)')
+                    response = await self.client.wait_for_message(author=message.author,check=self.checkEdit)
                     if response.content.startswith('abort'):
-                        await client.send_message(message.channel, 'Process aborted');
+                        await self.client.send_message(message.channel, 'Process aborted');
                         pass
                     else:
                         sql = """UPDATE MEMBERS SET username=%s 
                         WHERE ID=%s"""
-                        cur.execute(sql,[str(member),int(response.content)])
-                        await client.send_message(message.channel, 'Entry successfully updated');
+                        self.cur.execute(sql,[str(member),int(response.content)])
+                        await self.client.send_message(message.channel, 'Entry successfully updated');
                         
-        conn.commit()
+        self.conn.commit()
 
     @asyncio.coroutine
     async def closedb(self,channel):
-        conn.close()
-        await client.send_message(channel, 'Connection with database closed sir, ready for restart.');
+        self.conn.close()
+        await self.client.send_message(channel, 'Connection with database closed sir, ready for restart.');
         
     @asyncio.coroutine
     async def opendb(self,channel):
-        conn.close()
-        establishConnection()
-        await client.send_message(channel, 'Connection with database reestablished sir.');
+        self.conn.close()
+        self.establishConnection()
+        await self.client.send_message(channel, 'Connection with database reestablished sir.');
         
         
         
