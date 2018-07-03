6cb6c4a5d7ab9bc2386a8c41f3ef93e57962c3e9
annadess <a.bubnyak@gmail.com>
2018-07-03 15:40:20 +0200

    slpitting functions into modules

--- misc.py
+++ misc.py
@@ -4,7 +4,11 @@ import os
 import psycopg2
 import random
 
-class Misc(client):
+class Misc:
+
+    def __init__(self, client):
+        self.client = client
+        
     games = [discord.Game(name="detective",type=0),
 discord.Game(name="with Sumo",type=0),
 discord.Game(name="the android sent by CyberLife",type=0),
@@ -12,8 +16,6 @@ discord.Game(name="baseball with Hank",type=0),
 discord.Game(name="wake up Lieutenant",type=0),
 discord.Game(name="your buddy to drink with",type=0),
 discord.Game(name="blowing up Jericho",type=0)]
-    
-    client = client
     
     @asyncio.coroutine
     async def bestship(channel):
